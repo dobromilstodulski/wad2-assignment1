cdb41ad6a88975d5ef3412d082b809ff82f4543e
Dobromil Stodulski <bb100dulski@gmail.com>
2020-11-20 16:29:46 +0000

    Updated Movie Contexts

--- src/contexts/moviesContext.js
+++ src/contexts/moviesContext.js
@@ -1,5 +1,5 @@
 import React, { useEffect, createContext, useReducer } from "react";
-import { getMovies, getUpcomingMovies } from "../api/tmdb-api";
+import { getMovies, getUpcomingMovies, getLatestMovies, getTopRatedMovies } from "../api/tmdb-api";
 
 export const MoviesContext = createContext(null);
 
@@ -11,11 +11,17 @@ const reducer = (state, action) => {
           m.id === action.payload.movie.id ? { ...m, favorite: true } : m
         ),
         upcoming: [...state.upcoming],
+        latest: [...state.latest],
+        toprated: [...state.toprated],
       };
     case "load":
-      return { movies: action.payload.movies, upcoming: [...state.upcoming] };
+      return { movies: action.payload.movies, upcoming: [...state.upcoming], latest: [...state.latest],  toprated: [...state.toprated] };
     case "load-upcoming":
-      return { upcoming: action.payload.movies, movies: [...state.movies] };
+      return { upcoming: action.payload.movies, movies: [...state.movies], latest: [...state.latest],  toprated: [...state.toprated] };
+    case "load-latest":
+      return { latest: action.payload.movies, movies: [...state.movies], upcoming: [...state.upcoming],  toprated: [...state.toprated] };
+    case "load-toprated":
+      return { toprated: action.payload.movies, movies: [...state.movies], upcoming: [...state.upcoming], latest: [...state.latest] };
     case "add-review":
       return {
         movies: state.movies.map((m) =>
@@ -38,7 +44,7 @@ const reducer = (state, action) => {
 };
 
 const MoviesContextProvider = (props) => {
-  const [state, dispatch] = useReducer(reducer, { movies: [], upcoming: [] });
+  const [state, dispatch] = useReducer(reducer, { movies: [], upcoming: [], latest: [], toprated: [] });
 
   const addToFavorites = (movieId) => {
     const index = state.movies.map((m) => m.id).indexOf(movieId);
@@ -68,11 +74,27 @@ const MoviesContextProvider = (props) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  useEffect(() => {
+    getLatestMovies().then((movies) => {
+      dispatch({ type: "load-latest", payload: { movies } });
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    getTopRatedMovies().then((movies) => {
+      dispatch({ type: "load-toprated", payload: { movies } });
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <MoviesContext.Provider
       value={{
         movies: state.movies,
         upcoming: state.upcoming,
+        latest: state.latest,
+        toprated: state.toprated,
         addToFavorites: addToFavorites,
         addToWatchList: addToWatchList,
         addReview: addReview,
